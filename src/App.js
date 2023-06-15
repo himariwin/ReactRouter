df758d97a042f1ca45c880bf880dbef06bfd6733
himariwin <raigalvaao@gmail.com>
2023-06-15 17:08:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+
+// 1 - config react router
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+
+//components
+import Navbar from './components/Navbar';
+import SearchForm from './components/SearchForm';
+
+//pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Product from './pages/Product';
+import Info from './pages/Info';
+import NotFound from './pages/NotFound';
+import Search from './pages/Search';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Router</h1>
+      <BrowserRouter>
+      {/* 2 - links com react router */}
+        <Navbar />
+        {/* 9 - search params */}
+        <SearchForm />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/* 6 - nested routes */}
+          <Route path="/products/:id/info" element={<Info />} />
+          {/* 4 - rota dinâmica */}
+          <Route path="/products/:id" element={<Product />} />
+          {/* 9 - search params */}
+          <Route path="/search" element={<Search />} />
+          {/* 10 - redirect */}
+          <Route path="/company" element={<Navigate to="/about" />} />
+          {/* 7 - no match router */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <p>Sobre</p>
+    </div>
+  );
+};
+
+export default About;
